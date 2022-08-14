37d262975df7ee915297e674a55f2a4505400dbb
purnima1606 <purnimasingh26th@gmail.com>
2022-08-14 16:57:30 +0530

    1st

--- practice.py
+++ practice.py
@@ -41,3 +41,14 @@ l = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
 # Method Insertion Sort :-
+
+# for i in range(len(l)-1):
+#     if l[i] > l[i+1]:
+#         l[i], l[i+1] = l[i+1], l[i]
+#     for j in range(i, 0, -1):
+#         if l[j] < l[j-1]:
+#             l[j], l[j-1] = l[j-1], l[j]
+#
+# print(l)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
