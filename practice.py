1dbb4cf2fe44aee28d31370560594aff1e2a36b5
purnima1606 <purnimasingh26th@gmail.com>
2022-08-14 16:41:37 +0530

    1st

---
+++ practice.py
@@ -0,0 +1,43 @@
+# *********************************** Sorting Techniques ********************************************
+
+"""
+The different implementations of sorting techniques in Python are:
+
+1.Bubble Sort
+2.Selection Sort
+3.Insertion Sort : reverse check
+
+"""
+
+
+l = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+#
+# # Method Bubble sort :-
+#
+# for i in range(len(l)-1):
+#     for j in range(len(l)-1):
+#         print(l[j], l[j+1])
+#         if l[j] > l[j+1]:
+#             l[j],l[j+1] = l[j+1], l[j]
+#
+#
+#         # print(l)
+#
+# print(l)   # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+#
+# # Method Selection Sort :-
+
+
+# for  i in range(len(l)-1):
+#     for j in range(i+1, len(l)):
+#         if l[i] > l[j]:
+#             l[i], l[j] = l[j], l[i]
+#     print(l)
+
+# print(l)
+# # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# Method Insertion Sort :-
