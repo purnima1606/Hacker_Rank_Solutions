225d70c08f5d13987d4a5cf170695f79efea7a59
purnima1606 <purnimasingh26th@gmail.com>
2022-08-26 20:20:21 +0530

    1st

---
+++ Numpy/Dot_and_Cross.py
@@ -0,0 +1,8 @@
+"""
+"""
+import numpy
+N = int(input())
+A = numpy.array([list(map(int, input().split())) for _ in range(N)])
+B = numpy.array([list(map(int, input().split())) for _ in range(N)])
+print(numpy.dot(A, B))
+# print(numpy.cross(A, B))   # Error
