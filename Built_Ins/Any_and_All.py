d039364a3e6d16b82f8a28dd97e3c74e36c3659c
purnima1606 <purnimasingh26th@gmail.com>
2022-08-27 23:49:57 +0530

    1st

---
+++ Built_Ins/Any_and_All.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+# N = int(input())
+# arr = input().split()
+# print(any(i == i[::-1] for i in arr) and all(int(i)>0 for i in arr))
+
+# l = ["21" "22" "23"]
+# print(any(i == i[::-1] for i in l) and all(int(i)>0 for i in l))
+
+
